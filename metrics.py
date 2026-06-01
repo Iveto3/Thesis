400144d2c9a056d438dee8940e1b0f63dfa1c6f2
Iveto3 <i.i.ivanova@student.rug.nl>
2026-06-01 09:00:18 +0200

    fixing model initialization problem

--- metrics.py
+++ metrics.py
@@ -1,10 +1,13 @@
 from sentence_transformers import SentenceTransformer, util
 
+MODEL = SentenceTransformer(
+    "sentence-transformers/all-MiniLM-L6-v2",
+    device="cpu",
+)
+
 
 def compute_similarity(response: str, reference: str) -> float:
-    embeddings = SentenceTransformer(
-        "sentence-transformers/all-MiniLM-L6-v2"
-        ).encode(
+    embeddings = MODEL.encode(
         [response, reference],
         convert_to_tensor=True,
         normalize_embeddings=True,
